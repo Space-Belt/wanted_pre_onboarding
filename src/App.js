8dcba86d6712d1bb70b41a35b8f6d105b2d4812e
Space-Belt <dngurwn@gmail.com>
2022-02-08 17:28:05 +0900

    ✨ App.js AutoComplete추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Tab from "./component/Tab";
 import Toggle from "./component/Toggle";
 import Tag from "./component/Tag";
 import GlobalStyles from "./styles/GlobalStyles";
+import AutoComplete from "./component/AutoComplete";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Modal />
       <Tab />
       <Tag />
+      <AutoComplete />
     </>
   );
 }
